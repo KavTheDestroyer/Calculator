d90becad9b900aab4e6aa34bef18909e23412bd3
Kavya Khare <mac6@129mac18.valleychristian.net>
2017-01-12 10:49:41 -0800

    rounded calculator and attempting img in display

--- calculator.js
+++ calculator.js
@@ -2,7 +2,12 @@
 
 //Change display
 function d(val) {
+	if(val==''){
+		<img src="http://s3.amazonaws.com/feather-files-aviary-prod-us-east-1/h6x69kt5qn4mwje1/2017-01-12/7ee94255a16a45b18b0748d143686e5c.jpg" alt="Waves">
+	}
+	
 	document.getElementById("d").value = val;
+
 }
 
 //Type numbers and operators
